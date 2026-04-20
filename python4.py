ba820b4935afbe6b5514a8d693f7cb7da54af4d4
Mohammad Shahinur Rahman <shahinatorcse@email.com>
2026-04-20 07:30:41 +0600

    Task Manager Files Added

--- python4.py
+++
@@ -1,9 +0,0 @@
-"""
-Write a program that takes user input and appends it to a file named
-
-notes.txt without deleting existing content.
-
-"""
-text = input("Enter your note: ")
-with open("notes.txt", "a") as file:
-    file.write(text)
